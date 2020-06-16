f96448b7a870f0b9d31b3747a706d18f4fa20fe2
angiewambui <wambuinjenga44a2gmail.com>
2020-06-16 13:54:39 +0300

    added index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("div").toggle();
+//     });
+//   });
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("form").animate({
+        height: 'toggle'
+      });
+    });
+  });
