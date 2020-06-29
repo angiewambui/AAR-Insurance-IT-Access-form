d80d8bc96b3984354f37fbc357586f2fb172bd1f
angiewambui <wambuinjenga44a2gmail.com>
2020-06-29 15:34:46 +0300

    added functions

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,32 @@
       });
     });
   }
+  // function Getuserinfo(Name,Department,Jobtitle,Email,Personal ,Phone){
+// this.Name = name;
+// this.Department = Department;
+// this.Jobtitle = Jobtitle;
+// this.Email = Email;
+// this.Personal  = Personal ;
+// this.Phone = phone ;
+// }
+
+
+function getInfo()
+{
+  //to get userinfo
+var name = document.getElementById("name").value
+var NN = parseInt(name);
+var department = document.getElementById("dp").value;
+var DD = parseInt(department);
+var jobTitle = document.getElementById("jt").value
+var JT = parseInt(jobTitle);
+var email = document.getElementById("email").value
+var EM = parseInt(email);
+var personalNo = document.getElementById("pn").value
+var PP = parseInt(personalNo);
+var phoneNo = document.getElementById("phone").value
+var PP = parseInt(phoneNo);
+
+//to get user access requirments
+var userS = document.getElementById("permanent")
+var
